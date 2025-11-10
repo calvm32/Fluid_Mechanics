76faa923667b20dd77fe7968ae1bb82f8da52923
Calum Heldt <cal.muon32@gmail.com>
2025-11-10 00:39:01 -0600

    testing

--- solvers/create_timestep_solver.py
+++ solvers/create_timestep_solver.py
@@ -25,25 +25,13 @@ def create_timestep_solver(get_data, dsN, theta, u_old, u_new, make_weak_form,
     # Initialize coefficients
     f_n, g_n = get_data(0)
     f_np1, g_np1 = get_data(0)
-    idt = Constant(0)
+    idt = Constant(1.0)
 
     # Extract function space
     Z = u_new.function_space()
 
     # callable weak form
     weak_form = make_weak_form(theta, idt, f_n, f_np1, g_n, g_np1, dsN)
-
-    # Build weak form
-    if W is not None:
-        u, p = TrialFunctions(Z)
-        v, q = TestFunctions(Z)
-        u_old_v, p_old = split(u_old)
-        F = weak_form(u, p, u_old_v, p_old, v, q)
-    else:
-        u, v = TrialFunction(Z), TestFunction(Z)
-        F = weak_form(u, u_old, v)
-
-    a, L = lhs(F), rhs(F)
 
     def solve_(t, dt):
         """
@@ -54,6 +42,18 @@ def create_timestep_solver(get_data, dsN, theta, u_old, u_new, make_weak_form,
         get_data(t, (f_n, g_n))
         get_data(t+dt, (f_np1, g_np1))
         idt.assign(1/dt)
+
+        # Build weak form
+        if W is not None:
+            u, p = TrialFunctions(Z)
+            v, q = TestFunctions(Z)
+            u_old_v, p_old = split(u_old)
+            F = weak_form(u, p, u_old_v, p_old, v, q)
+        else:
+            u, v = TrialFunction(Z), TestFunction(Z)
+            F = weak_form(u, u_old, v)
+
+        a, L = lhs(F), rhs(F)
 
         # Run the solver
         solve(a == L, u_new)
