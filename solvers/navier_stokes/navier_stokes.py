49a2bea31555e4b24c980cd2a0da97f8c8182239
Calum Heldt <cal.muon32@gmail.com>
2025-11-09 20:12:32 -0600

    fix

--- solvers/navier_stokes/navier_stokes.py
+++ solvers/navier_stokes/navier_stokes.py
@@ -100,10 +100,10 @@ solver_parameters = {
 }
 
 # run
-timestepper(Z, ds(1), theta, T, dt, u0, get_data, make_weak_form,
+timestepper(V, ds(1), theta, T, dt, u0, get_data, make_weak_form,
         bcs=bcs, nullspace=nullspace, solver_parameters=solver_parameters, 
         appctx=appctx, W=W)
 
-timestepper_adaptive(Z, ds(1), theta, T, tol, u0, get_data, make_weak_form,
+timestepper_adaptive(V, ds(1), theta, T, tol, u0, get_data, make_weak_form,
         bcs=bcs, nullspace=nullspace, solver_parameters=solver_parameters, 
         appctx=appctx, W=W)
