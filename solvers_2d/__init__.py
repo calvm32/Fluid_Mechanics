e0fce21ac3465ff37d46a0512b8d6fc2a16f9143
Calum Heldt <cal.muon32@gmail.com>
2025-11-12 19:20:20 -0600

    all adaptive removed now

--- solvers_2d/__init__.py
+++ solvers_2d/__init__.py
@@ -12,14 +12,11 @@ We perform theta-scheme discretization, i.e.
 
 Modules:
     - timestepper.py: fixed-step theta-scheme time integrator
-    - timestepper_adaptive.py: adaptive time-stepping version
 
 """
 
 from .timestepper import timestepper
-from .timestepper_adaptive import timestepper_adaptive
 
 __all__ = [
     "timestepper",
-    "timestepper_adaptive",
 ]
