214bdde3c6a7d68318fa28b3e263f6b37b199e65
Calum Heldt <cal.muon32@gmail.com>
2025-12-03 13:52:59 -0600

    dont pass any appctx

--- solvers_2d/navier_stokes/solver_MMS.py
+++ solvers_2d/navier_stokes/solver_MMS.py
@@ -24,12 +24,6 @@ for N in N_list:
     W = FunctionSpace(mesh, "CG", 1)
     Z = V * W
 
-    # pass velocity space
-    solver_parameters["appctx"]["velocity_space"] = Z.sub(0).collapse()
-    solver_parameters["appctx"]["pressure_space"] = Z.sub(1).collapse()
-
-    print("Z block sizes:", Z.dof_dset().sizes)   # or inspect Firedrake block sizes
-
     # time dependant
     def get_data(t):
         
