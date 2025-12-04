ee16cfe3a38a53848f6925ec4c4328fc0d0980d0
Calum Heldt <cal.muon32@gmail.com>
2025-12-03 20:00:22 -0600

    try again

--- solvers_2d/navier_stokes/solver.py
+++ solvers_2d/navier_stokes/solver.py
@@ -147,16 +147,16 @@ def make_weak_form(theta, idt, f, f_old, g, g_old, dx , dsN):
     f_mid = theta * f + (1-theta) * f_old
     g_mid = theta * g + (1-theta) * g_old
 
-    def F(u, u_old, v):
-        u, p = split(u_new)
+    def F(u_new, u_old, v):
+        u_new, p_new = split(u_new)
         u_old_, p_old = split(u_old)
         v_u, v_p = split(v)
 
-        u_mid = theta * u + (1 - theta) * u_old
+        u_mid = theta * u_new + (1 - theta) * u_old
     
         return (
             # Time derivative
-            idt*inner(u - u_old_, v_u)*dx
+            idt*inner(u_new - u_old_, v_u)*dx
 
             # Diffusion
             + (1/Re)*inner(grad(u_mid), grad(v_u))*dx
@@ -165,7 +165,7 @@ def make_weak_form(theta, idt, f, f_old, g, g_old, dx , dsN):
             + inner(dot(u_mid, grad(u_mid)), v_u)*dx
 
             # Pressure
-            - inner(p, div(v_u))*dx
+            - inner(p_new, div(v_u))*dx
             + inner(div(u_mid), v_p)*dx
 
             # Source, boundary
